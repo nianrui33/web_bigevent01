f3252a8143ad9af8a0f24602ce929fc757365520
nianrui <807837114@qq.com>
2022-05-27 18:52:04 +0800

    完成文章管理相关功能的开发

---
+++ assets/js/article/art_list.js
@@ -0,0 +1,139 @@
+$(function () {
+    var layer = layui.layer
+    var form = layui.form
+    var laypage = layui.laypage
+
+    // 美化时间的过滤器
+    template.defaults.imports.dataFormat = function (date) {
+        const dt = new Date(date)
+        var y = dt.getFullYear()
+        var m = padZero(dt.getMonth() + 1)
+        var d = padZero(dt.getDate())
+
+        var hh = padZero(dt.getHours())
+        var mm = padZero(dt.getMinutes())
+        var ss = padZero(dt.getSeconds())
+
+        return y + '-' + m + '-' + d + ' ' + hh + ':' + mm + ':' + ss
+
+    }
+
+    // 定义补0函数
+    function padZero(n) {
+        return n > 9 ? n : '0' + n
+    }
+
+
+    var q = {
+        pagenum: 1,
+        pagesize: 2,
+        cate_id: '',
+        state: ''
+    }
+
+    initTable()
+    initCate()
+    function initTable() {
+        $.ajax({
+            method: 'GET',
+            url: '/my/article/list',
+            data: q,
+            success: function (res) {
+                console.log(res);
+                if (res.status !== 0) {
+                    return layer.msg('获取文章列表失败！')
+                }
+
+                var htmlStr = template('tpl-table', res)
+                $('tbody').html(htmlStr)
+                renderPage(res.total)
+
+            }
+        })
+    }
+
+
+    // 初始化文章分类方法
+    function initCate() {
+        $.ajax({
+            method: 'GET',
+            url: '/my/article/cates',
+            success: function (res) {
+                if (res.status !== 0) {
+                    return layer.msg('获取分类数据失败！')
+                }
+
+                var htmlStr = template('tpl-cate', res)
+                $('[name=cate_id]').html(htmlStr)
+                form.render()
+            }
+        })
+    }
+
+    $('#form-search').on('submit', function (e) {
+        e.preventDefault()
+        var cate_id = $('[name=cate_id]').val()
+        var state = $('[name=state]').val()
+        q.cate_id = cate_id
+        q.state = state
+        initTable()
+    })
+
+    // 定义渲染分页的方法
+    function renderPage(total) {
+
+        //执行一个laypage实例
+        laypage.render({
+            
+            elem: 'pageBox', //
+            count: total, //数据总数，从服务端得到
+            limit: q.pagesize,
+            curr: q.pagenum,
+            layout: ['count', 'limit', 'prev', 'page', 'next', 'skip'],
+            limits:[2,3,5,10],
+            jump: function (obj,first) {
+                //obj包含了当前分页的所有参数，比如：
+                console.log(obj.curr); //得到当前页，以便向服务端请求对应页的数据。
+                console.log(first);
+                q.pagenum = obj.curr
+                q.pagesize= obj.limit
+                if (!first) {
+                    initTable()
+                }
+             
+            }
+        })
+
+
+    }
+
+    $('tbody').on('click', function () {
+        // console.log('ok');
+        var len = $('.btn-delete').length
+        console.log(len);
+        var id = $(this).attr('data-id')
+        layer.confirm('确认删除?', {icon: 3, title:'提示'}, function(index){
+            $.ajax({
+                method: 'GET',
+                url: '/my/article/deletecate/' + id,
+                success: function (res) {
+                    if (res.status !== 0) {
+                        return layer.msg('删除文章失败！')
+                    }
+                    layer.msg('删除文章成功！')
+                    if (len === 1) {
+                        q.pagenum = q.pagenum === 1? 1: q.pagenum-1
+                        
+                    }
+
+                    initTable()
+                }
+           })
+            
+            layer.close(index);
+        });
+        
+
+    })
+
+})
